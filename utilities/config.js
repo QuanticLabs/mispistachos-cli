45101654b94f56a2b27e5cafd1c6bcfca8652a4f
Gonzalo Lopez <gslopez@uc.cl>
2017-01-05 19:28:50 -0300

    se pueden configurar permisos y contraseñas y se guardan en archivos .json

--- utilities/config.js
+++ utilities/config.js
@@ -1,11 +1,64 @@
 #!/usr/bin/env node
 
+var jsonFile = require('jsonfile')
+var cmd_ = require('./cmd.js');
 
 var config = function(){
-  var folder = process.env.HOME + "/.mp"
 
-  var checkFolderOrCreate = function(){
+  var mainFolder = process.env.HOME + "/.mp"
 
+  var paths = {
+    k8s: mainFolder + "/k8s.json"
+    gcloud: mainFolder + "/gcloud.json"
+  }
+
+  this.values = {
+    k8s: {
+
+    },
+    gcloud: {
+      projectId: "XXXX",
+      clusterName: "XXXX"
+    }
+  }
+
+  var cmd = new cmd_.Cmd()
+
+  var checkConfigOrCreate = function(){
+    cmd.sync("mkdir -p " + mainFolder, null)
+    cmd.sync("touch " + paths.k8s, null)
+    cmd.sync("touch " + paths.gcloud, null)
+  }
+
+  var read = function(filePath){
+    var config = jsonFile.readFileSync(filePath)
+    if (config === undefined || config === null)
+      return {}
+    return config
+  }
+
+  this.reload = function(fileId){
+    var filePath = paths[fileId]
+    this.values[fileId] = read(filePath)
+  }
+
+  this.reloadAll = function(){
+    var keys = Object.keys(paths)
+    for (var key in keys) {
+        this.reload(key)
+    }
+  }
+
+  this.save = function(fileId){
+    var filePath = paths[fileId]
+    jsonfile.writeFileSync(filePath, this.values[fileId])
+  }
+
+  this.saveAll = function(){
+    var keys = Object.keys(paths)
+    for (var key in keys) {
+        this.save(key)
+    }
   }
 
 }
